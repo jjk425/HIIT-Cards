c6c9e68e99569dfe1177707f61f1af61dba869fb
Josh Kogan <jjk425@stern.nyu.edu>
2015-06-13 18:40:01 -0400

    Created templates for Stats/Results activity

---
+++ app/src/main/java/com/example/josh/hiitcards/ShowResults.java
@@ -0,0 +1,10 @@
+package com.example.josh.hiitcards;
+
+import android.app.Activity;
+import android.widget.TextView;
+
+/**
+ * Created by Josh on 6/11/15.
+ */
+public class ShowResults extends Activity {
+}
